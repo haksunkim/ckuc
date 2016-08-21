f961e31485f889e3f652b2e96b783e3648ac26c5
Haksun Kim <itpro@haksunkim.com>
2016-08-22 09:54:42 +1000

    updated Devise to use username to login and password reset
    - updated Devise to use username for login
    - updated Devise to use username for password reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818034606) do
+ActiveRecord::Schema.define(version: 20160819133907) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "album"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160818034606) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
