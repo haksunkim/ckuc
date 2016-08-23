ed2898fdac5b324f61c665b2bb0950080f1616d2
Haksun Kim <itpro@haksunkim.com>
2016-08-23 12:50:08 +1000

    ckeditor with full toolbar

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -40,7 +40,7 @@ class TranslationsController < ApplicationController
     if @translation.update(translation_params)
       flash[:notice] = "Translation for #{ @key } updated."
       I18n.backend.reload!
-      redirect_to locale_translations_url(@locale, :params => { key: params[:key] })
+      redirect_to locale_translations_url(@locale, :params => { key: translation_params[:key].split('.').first })
     else
       render :edit
     end
